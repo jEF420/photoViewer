1234bbf1e748d9293155c8a14f9e48aa700bdd9c
mohammedalialhilo <49024733+mohammedalialhilo@users.noreply.github.com>
2019-09-29 13:36:26 +0200

    email validator done

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,14 @@ const sendBtn = document.querySelector('#Submit');
 var info = new Array();
 sendBtn.addEventListener('click', submit());
 
-function submit() {
-
+function validateemail()  
+{  
+    
+    var x=document.myform.email.value;  
+    var atposition=x.indexOf("@");  
+    var dotposition=x.lastIndexOf(".");  
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+      alert("Please enter a valid e-mail address");  
+      return false;  
+      }  
 }
