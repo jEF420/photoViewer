7c3c7f26d8bd8be81d799a94eab55e889f207e90
mohammedalialhilo <49024733+mohammedalialhilo@users.noreply.github.com>
2019-09-29 13:38:01 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,12 @@ sendBtn.addEventListener('click', submit());
 
 function validateemail()  
 {  
-    
+
     var x=document.myform.email.value;  
     var atposition=x.indexOf("@");  
     var dotposition=x.lastIndexOf(".");  
     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
       alert("Please enter a valid e-mail address");  
       return false;  
-      }  
+      }
 }
