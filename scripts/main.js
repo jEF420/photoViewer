31d44e3eae0468fce9912c731b2dea708f9d794c
Mohammed Ali <alhiloempire@gmail.com>
2019-09-30 11:30:15 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,26 @@
-const sendBtn = document.querySelector('#Submit');
-var info = new Array();
-sendBtn.addEventListener('click', submit());
+const fname = document.querySelector('#fname');
+const lname = document.querySelector('#lname');
+const subBtn = document.querySelector('#submit');
+const email = document.querySelector('#email');
+var save = new Array();
 
-function validateemail()  
-{  
 
-    var x=document.myform.email.value;  
-    var atposition=x.indexOf("@");  
-    var dotposition=x.lastIndexOf(".");  
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-      alert("Please enter a valid e-mail address");  
-      return false;  
-      }
+function validateemail() {
+
+    var x = document.myform.email.value;
+    var atposition = x.indexOf("@");
+    var dotposition = x.lastIndexOf(".");
+    var y = document.myform.firstname.value;
+    var z = document.myform.lastname.value;
+    var info = "Name: " + y + " " + z + "\n" + "Email: " + x;
+    if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+        alert("Please enter a valid e-mail address");
+        email.style.border = " groove #FF0000";
+        return false;
+    } else {
+        save.push(info);
+        console.log("\n" + save + "\n");
+
+    }
+
 }
