b63422ebf18175ea60600b1e093d38014d3b49b3
jEF <alexxander.00@outlook.com>
2019-10-02 13:02:32 +0200

    försök fixa

--- scripts/slider.js
+++ scripts/slider.js
@@ -33,3 +33,44 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " w3-opacity-off";
 }
+
+var a = document.querySelector("img");
+
+
+function nameCheck(input){
+  if (input == "nature"){
+     for (let i = 0; i < a[i]; i++) {
+       var strng = a[i].getAttribute("src");
+       var n = strng.includes("nature");
+       if (n===true){
+         a.getAttribute("src").includes("nature").style.display = "block";
+         a.getAttribute("src").includes("city").style.display = "none";
+         a.getAttribute("src").includes("ca").style.display = "none";
+       }     
+     }
+     console.log(strng);
+  }
+  else if (input == "city"){
+    for (let i = 0; i < a[i]; i++) {
+      var strng = a[i].getAttribute("src");
+      var m = strng.includes("city");
+      if (m==true){
+        a.getAttribute("src").includes("nature").style.display = "none";
+        a.getAttribute("src").includes("city").style.display = "block";
+        a.getAttribute("src").includes("ca").style.display = "none";
+      }     
+    }
+    
+ }
+ else if (input == "ca"){
+  for (let i = 0; i < a[i]; i++) {
+    var strng = a[i].getAttribute("src");
+    var m = strng.includes("ca");
+    if (m==true){
+      a.getAttribute("src").includes("nature").style.display = "none";
+      a.getAttribute("src").includes("city").style.display = "none";
+      a.getAttribute("src").includes("ca").style.display = "block";
+    }     
+  }
+}
+}
